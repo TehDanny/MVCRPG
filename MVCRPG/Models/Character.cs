fcf2cebd5fd14b71878e95085c9e096dcf29c7bb
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-12-01 13:06:41 +0100

    Upstart

---
+++ MVCRPG/Models/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCRPG.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        private const int defaultHealth = 100;
+
+        public Character(string name)
+        {
+            Name = name;
+            Health = defaultHealth;
+        }
+    }
+}
---
+++ MVCRPG/Models/Equipment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCRPG.Models
+{
+    public class Equipment
+    {
+    }
+}
